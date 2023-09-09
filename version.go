07dbf9a9db6a41ddb3d7e943d37b4518d6c00298
whatwewant <tobewhatwewant@outlook.com>
2023-09-09 23:59:52 +0800

    chore(release): bumped version to v0.7.5

--- version.go
+++ version.go
@@ -5,7 +5,7 @@ import (
 )
 
 // Version the version of supervisor
-var Version = "0.7.4"
+var Version = "0.7.5"
 
 // VersionCommand implement the flags.Commander interface
 type VersionCommand struct {
